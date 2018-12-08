6559ba0e84068f431e41d174c138d6a127fa3cb3
Jan <nowakjanmail@yahoo.com>
2018-12-08 11:28:53 +0100

    init

---
+++ zadanie1.py
@@ -0,0 +1,15 @@
+# Policz pole trojkąta o podstawie 10 i wysokosci 5
+
+print(" Pole trojkata wynosi", (10 * 5)/2)
+
+# Policz pole koła o promieniu 7
+
+print(" Pole koła wynosi", 3.14 * 7 ** 2)
+
+# Policz pole trapezu o długości podstaw 3 i 9 oraz wysokości 5
+
+print(" Pole trapezu wynosi", ((3 + 9) * 5)/2)
+
+# Policz objętość kuli o zadanym promieniu
+
+print(" Objętość kuli wynosi", 4/3 * 3.14 * 7 ** 3)
